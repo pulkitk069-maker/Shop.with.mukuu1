321fc40a668369a48b843af9a3cc78fc1fef6916
pulkitk069-maker <pulkitk069@gmail.com>
2025-12-02 17:27:23 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth"; // Ye line add ki hai
 
-// Aapki Original Keys jo aapne bheji thin
+// Aapki keys (Same rahengi)
 const firebaseConfig = {
   apiKey: "AIzaSyCFEqycZonzVZnfRBjBUZpx8fA1Vrz3m5A",
   authDomain: "shop-with-mukuu.firebaseapp.com",
@@ -11,6 +12,6 @@ const firebaseConfig = {
   appId: "1:814428951768:web:a9e43db094cbfa7410f3ad"
 };
 
-// App start karna
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app); // Ye naya export hai login ke liye
